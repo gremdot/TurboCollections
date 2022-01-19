33b84dfb90877a1098d0a54d1cc361267a2bd858
GREM <naomiruokamo@gmail.com>
2022-01-19 17:04:26 +0100

    Following the Code Along and adding all the features excluding the extra tests

--- CustomerManagement/Program.cs
+++ CustomerManagement/Program.cs
@@ -8,6 +8,14 @@ namespace CustomerManagement
         static void Main(string[] args)
         {
             var turbolist = new TurboList<float>();
+            turbolist.Add(3f);
+            turbolist.Add(-7f);
+
+
+            foreach (var item in turbolist)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
